9df61d7fc8faec7d914bfba3304dc4b4adcd4c17
Fran Moreno <fran.moreno.se@gmail.com>
2024-09-03 07:58:06 +0200

    Added some models with admin site

--- catalog/admin.py
+++ catalog/admin.py
@@ -2,9 +2,23 @@ from django.contrib import admin
 
 from .models import Author, Genre, Book, BookInstance, Language, Review
 
-admin.site.register(Book)
-admin.site.register(Author)
 admin.site.register(Genre)
-admin.site.register(BookInstance)
 admin.site.register(Language)
 admin.site.register(Review)
+
+
+class AuthorAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Author, AuthorAdmin)
