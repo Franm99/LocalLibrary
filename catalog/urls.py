66bd3b71ba7d5d3c9c4fc72d1dcdfc2cd8836629
Fran Moreno <fran.moreno.se@gmail.com>
2024-09-29 14:34:30 +0200

    Added form views for managing authors and books

--- catalog/urls.py
+++ catalog/urls.py
@@ -10,4 +10,10 @@ urlpatterns = [
     path('mybooks/', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
     path('staff/allbooks', views.AllLoanedBooksListView.as_view(), name='all-borrowed'),
     path('book/<uuid:pk>/renew/', views.renew_book_librarian, name='renew-book-librarian'),
+    path('author/create/', view=views.AuthorCreate.as_view(), name='author-create'),
+    path('author/<int:pk>/update', view=views.AuthorUpdate.as_view(), name='author-update'),
+    path('author/<int:pk>/delete/', view=views.AuthorDelete.as_view(), name='author-delete'),
+    path('book/create/', view=views.BookCreate.as_view(), name='book-create'),
+    path('book/<int:pk>/update', view=views.BookUpdate.as_view(), name='book-update'),
+    path('book/<int:pk>/delete/', view=views.BookDelete.as_view(), name='book-delete'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -5,9 +5,11 @@ from typing import Any
 from django.db.models.query import QuerySet
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from django.contrib.auth.decorators import login_required, permission_required
 
 from .models import Book, Author, BookInstance, Genre
 from .forms import RenewBookForm
@@ -130,6 +132,8 @@ class AllLoanedBooksListView(LoginRequiredMixin, PermissionRequiredMixin, ListVi
         )
         
         
+@login_required
+@permission_required('catalog.can_mark_returned', raise_exception=True)
 def renew_book_librarian(request, pk):
     book_instance = get_object_or_404(BookInstance, pk=pk)
     
@@ -159,4 +163,59 @@ def renew_book_librarian(request, pk):
     }
     
     return render(request, 'catalog/book_renew_librarian.html', context)
+
+
+class AuthorCreate(PermissionRequiredMixin, CreateView):
+    model = Author
+    fields = ['first_name', 'last_name', 'date_of_birth', 'date_of_death']
+    initial = {'date_of_death': '11/11/2023'}
+    permission_required = 'catalog.add_author'
     
+
+class AuthorUpdate(PermissionRequiredMixin, UpdateView):
+    model = Author
+    # NOTE: Not recommended - Potential security issue in case that new fields are added in the future!
+    fields = '__all__'
+    permission_required = 'catalog.change_author'
+    
+    
+class AuthorDelete(PermissionRequiredMixin, DeleteView):
+    model = Author
+    success_url = reverse_lazy('authors')
+    permission_required = 'catalog.delete_author'
+    
+    def form_valid(self, form):
+        try:
+            self.object.delete()
+            return HttpResponseRedirect(self.success_url)
+        except Exception as e:
+            return HttpResponseRedirect(
+                reverse("author-delete", kwargs={"pk": self.object.pk})
+            )
+            
+
+class BookCreate(PermissionRequiredMixin, CreateView):
+    model = Book
+    fields = ['title', 'author', 'summary', 'isbn', 'genre', 'language' ]
+    permission_required = 'catalog.add_book'
+    
+
+class BookUpdate(PermissionRequiredMixin, UpdateView):
+    model = Book
+    fields = ['title', 'author', 'summary', 'isbn', 'genre', 'language' ]
+    permission_required = 'catalog.change_book'
+    
+    
+class BookDelete(PermissionRequiredMixin, DeleteView):
+    model = Book
+    success_url = reverse_lazy('books')
+    permission_required = 'catalog.delete_book'
+    
+    def form_valid(self, form):
+        try:
+            self.object.delete()
+            return HttpResponseRedirect(self.success_url)
+        except Exception as e:
+            return HttpResponseRedirect(
+                reverse("book-delete", kwargs={"pk": self.object.pk})
+            )
